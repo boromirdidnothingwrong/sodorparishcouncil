1e315464810b356e08e5587b041ea5461abe7e5d
boromirdidnothingwrong <150016935+boromirdidnothingwrong@users.noreply.github.com>
2023-11-21 09:08:53 +0000

    Commit

--- panther/src/routes/+layout.svelte
+++ panther/src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
   import "../app.pcss";
+  import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
 <slot />
--- panther/src/routes/+page.svelte
+++ panther/src/routes/+page.svelte
@@ -1,15 +1,9 @@
-import { AppShell } from '@skeletonlabs/skeleton';
-
-<AppShell>
-	<svelte:fragment slot="header">Header</svelte:fragment>
-	<!-- (sidebarLeft) -->
-	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
-	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
-	<!-- Router Slot -->
-	<slot />
-	<!-- ---- / ---- -->
-	<!-- (pageFooter) -->
-	<svelte:fragment slot="footer">Footer</svelte:fragment>
-</AppShell>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+</div>
