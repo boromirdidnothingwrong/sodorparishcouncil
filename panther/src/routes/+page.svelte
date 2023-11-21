df1cfea828e5273470c9178f0f5892eb997b8a1a
boromirdidnothingwrong <150016935+boromirdidnothingwrong@users.noreply.github.com>
2023-11-21 09:00:34 +0000

    Commit

--- panther/src/routes/+page.svelte
+++ panther/src/routes/+page.svelte
@@ -1,2 +1,15 @@
+import { AppShell } from '@skeletonlabs/skeleton';
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
