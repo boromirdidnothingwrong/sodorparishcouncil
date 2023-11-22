d9cb6079869c2ff9ecb45586eb6eabd3b7ec3359
boromirdidnothingwrong <josh.thompson@staffordshire.gov.uk>
2023-11-22 13:32:00 +0000

    Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { AppShell } from '@skeletonlabs/skeleton';
+  
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
+<slot />
