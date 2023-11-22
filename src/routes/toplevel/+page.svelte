8a57e3fa51c16a156deb08fc33b3eb4c0e5bc222
boromirdidnothingwrong <josh.thompson@staffordshire.gov.uk>
2023-11-22 19:53:29 +0000

    Commit

---
+++ src/routes/toplevel/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+    <title>Main Page</title>
+</svelte:head>
+<script>
+   import { item } from "./+page.js"
+</script>
+
+<div>
+    <h1>{item}</h1>
+
+    {#each item.quote as quote}
+  <div>
+    <p>{quote}</p>
+  </div>
+{/each}
+</div>
