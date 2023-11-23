b10c70eee73644ca7df8f2e63b8dd442bf328bef
Vasculum <50236924+Vasculum@users.noreply.github.com>
2023-11-23 14:44:19 +0000

    Things broke after the last commit....

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,36 @@
 <script lang="ts" src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js">
-	import '../app.postcss';
-	import { AppShell, AppBar, AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import "../app.pcss";
+    import '../app.postcss';
+    import { AppShell, AppBar, AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 
-	import { page } from '$app/stores';
+    import { page } from '$app/stores';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
-	
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+    // Floating UI for Popups
+    import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+    import { storePopup } from '@skeletonlabs/skeleton';
+    import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+
+    storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 
-   /** @type {import('./$types').PageData} */
-	export let data;
-  
-	// For the Tabs
-	let tabSet: number = 0;
+    /** @type {import('./$types').PageData} */
+    export let data;
+
+    // For the Tabs
+    let tabSet: number = 0;
 </script>
 
 <!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<AppBar gridcolumns="grid-cols-3" slotdefault="place-self-center" slottrail="place-content-end">
 		<svelte:fragment slot="lead">Some Icon</svelte:fragment>
 		Page Title Placeholder
 		<!-- Josh needs to figure out how the hell to dynamically load the page title. Nightmare for some reason!-->
 		<svelte:fragment slot="trail">(actions)</svelte:fragment>
 	</AppBar>
 	<TabGroup>
-		<TabAnchor href="/" selected={$page.url.pathname === '/'}>Home</TabAnchor>
-		<TabAnchor href="[id]/about" selected={$page.url.pathname === '[id]/about'}>About</TabAnchor>
+		<TabAnchor href="/" selected="{$page.url.pathname===''}>Home</TabAnchor>
 	</TabGroup>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
@@ -39,7 +39,7 @@
 	<!-- (pageHeader) -->
 	<!-- Router Slot -->
 	
-	<slot />
+	<slot></slot>
 	<!-- ---- / ---- -->
 	<!-- (pageFooter) -->
 	<!-- (footer) -->
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,27 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin';
-import { myCustomTheme } from './my-custom-theme';
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 
-export default {
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
+const config = {
+	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		forms,
-		typography,
-		skeleton({
-				themes: {
-					custom: [
-						myCustomTheme
-					]
-				}
-			})
-		]
-	
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton
+	]
 } satisfies Config;
+
+export default config;
