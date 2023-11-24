b74a3e9bf44a01a43fa1b2ad7765768c7dcce924
Vasculum <50236924+Vasculum@users.noreply.github.com>
2023-11-24 08:40:00 +0000

    Sort of working. Can merge from here

--- panther/src/routes/+layout.svelte
+++ panther/src/routes/+layout.svelte
@@ -1,6 +1,45 @@
 <script>
-    import "../app.css";
+  import "../app.pcss";
+  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    
+  /*Initialise the Contensis Delivery API*/
+  const { Client } = require("contensis-delivery-api");
+  const ContensisClient = Client.create({
+    rootUrl:"staffscc",
+    accessToken: "access_token",
+    projectId: "testingJosh",
+  })
+
 </script>
-  
-  
-<slot />
+
+<AppShell>
+	<svelte:fragment slot="pageHeader">
+    <AppBar>Hold for Title</AppBar>
+  </svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
+
+<style>
+.container {
+  width: 400px;
+  height: 300px;
+  background-color: #ccc;
+  position: absolute;
+  /*it can be fixed too*/
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  /*this to solve "the content will not be cut when the window is smaller than the content": */
+  max-width: 100%;
+  max-height: 100%;
+  overflow: auto;
+}
+
+</style>
--- panther/src/routes/+page.svelte
+++ panther/src/routes/+page.svelte
@@ -1,14 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<div class="container"><h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <h1 class="text-3xl font-bold underline">
     Hello world!
 </h1>
+<div>
 
-<button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button1</button>
-<button class="btn btn-secondary">Two</button>
-<style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-</style>
-  
+</div></div>
